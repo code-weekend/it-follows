72558ae978f9aa00acdbcc8215c62190a06c4c74
Marco Antônio <ma.souza.junior@gmail.com>
2018-08-26 16:28:46 -0300

    FEA : Get roration angule

--- utils.js
+++ utils.js
@@ -11,11 +11,26 @@ const diffPosition = (p1, p2) => ({
     y: p1.y - p2.y,
 })
 
-const start = (p1, p2) => ({
-    x: p1.x - p2.x,
-    y: p1.y - p2.y,
-})
 
 const getRandInt = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min)
+}
+
+const vectorMod = (vector)=> Math.sqrt(vector.x**2 + vector.y**2)
+
+const getRotation = (p1, p2)=>{
+    let hipotenusa = diffPosition(p1,p2)
+    let modHipo = vectorMod(hipotenusa)
+    let modCatY = p1.y - p2.y
+    let modCatX = p1.x - p2.x
+    let seno = (modCatY / modHipo)
+    let cosseno = (modCatX / modHipo)
+    let angule
+    if(seno){
+        angule = Math.asin(seno)
+
+    }else{
+        angule = Math.acos(cosseno)
+    }
+    console.log(angule);
 }
